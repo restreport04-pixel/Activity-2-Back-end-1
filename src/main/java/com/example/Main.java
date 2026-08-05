a9d7bb705c67b54a5a10f753536bb2adf3a7577f
Thomas Restrepo Rodríguez <restreport04@gmail.com>
2026-08-05 16:05:27 -0500

    Parte 3

--- src/main/java/com/example/Main.java
+++ src/main/java/com/example/Main.java
@@ -2,6 +2,16 @@ package com.example;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+
+        Libro l1 = new Libro();
+        l1.mostrarDetalles();
+
+        Libro l2 = new Libro("Harry Potter","J.K. Rowling");
+        l2.mostrarDetalles();
+
+        Libro l3 = new Libro("El Señor de los Anillos","J.R.R. Tolkien",1200 );
+        l3.mostrarDetalles();
+
+
     }
 }
