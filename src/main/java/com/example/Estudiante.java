fc9ff5dee965aae78a7c3332cf976073c39455ae
Thomas Restrepo Rodríguez <restreport04@gmail.com>
2026-08-06 07:51:01 -0500

    Parte 5

--- src/main/java/com/example/Estudiante.java
+++ src/main/java/com/example/Estudiante.java
@@ -2,4 +2,32 @@ package com.example;
 
 public class Estudiante {
 
+    private String nombre;
+    private int edad;
+    private double promedio;
+
+
+    public Estudiante(String nombre, int edad, double promedio) {
+        this.nombre = nombre;
+        setEdad(edad); // Usamos el setter para validar la edad desde el inicio
+        this.promedio = promedio;
+    }
+
+    // Setter con validación
+    public void setEdad(int edad) {
+        if (edad>0)
+        {
+            this.edad = edad;
+        }
+    }
+
+    // Método que devuelve un BOOLEAN (true o false)
+    public boolean haAprobado() {
+        return promedio >= 3.0; // Devuelve true si sacó 3 o más
+    }
+
+    public void mostrarInfo() {
+        String estado = haAprobado() ? "APROBADO" : "REPROBADO";
+        System.out.println("Estudiante: " + nombre + " | Promedio: " + promedio + " | Estado: " + estado);
+    }
 }
